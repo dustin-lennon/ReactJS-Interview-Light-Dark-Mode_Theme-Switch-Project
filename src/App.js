a1fd69da05995e4f1de6347bbace64449f9bdb99
Dustin Lennon <dustin.lennon@stelth2000inc.com>
2024-05-31 15:44:27 -0400

    Theme switcher using local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LightDarkMode from './components/light-dark-mode';
 
 function App() {
   return (
     <div className="App">
-
+      {/* Light & Dark Mode Theme Switch Component */}
+      <LightDarkMode />
     </div>
   );
 }
